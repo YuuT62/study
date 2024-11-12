35d1cd4fb8b0757278318ff60f9669b6df7345e8
yu <y.takada.0602@gmail.com>
2024-11-12 14:24:04 +0900

    conflict01

--- src/resources/views/test2.blade.php
+++ src/resources/views/test2.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-         @vite(['resources/css/app.css', 'resources/js/app.js'])
+         <p>feature/conflict01で修正したよ</p>
     </head>
     <body>
         
