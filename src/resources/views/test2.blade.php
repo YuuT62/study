c0bda1c0d2c0c5f7d522ed2f046644f72352caaf
yu <y.takada.0602@gmail.com>
2024-11-12 14:28:08 +0900

    test

--- src/resources/views/test2.blade.php
+++ src/resources/views/test2.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-         <p>feature/conflict01で修正したよ</p>
+         <p>このコードをコンフリクトさせるよ</p>
     </head>
     <body>
         
